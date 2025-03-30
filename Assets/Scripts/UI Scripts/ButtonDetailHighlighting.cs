918d43c6b226f5b02adc67c759951de8de5df817
nkoles <37773744+nkoles@users.noreply.github.com>
2025-03-30 22:47:28 +0100

    ActionHighlighting Working

---
+++ Assets/Scripts/UI Scripts/ButtonDetailHighlighting.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonDetailHighlighting : MonoBehaviour
+{
+    static public UnityEvent<int> ButtonSelected = new UnityEvent<int>();
+    static public UnityEvent ButtonDeselected = new UnityEvent();
+
+    static public UnityEvent ChoosingAction = new UnityEvent();
+    static public UnityEvent ChoseAction = new UnityEvent();
+
+    public TextMeshProUGUI buttonDescriptionTMProTarget;
+    public float textSpeed;
+
+    private int _lastActivatedTextID;
+    private Coroutine _typingRoutine;
+
+    [Header("0 = Attack, 1 = Passive, 2 = Affirm, 3 = Confront")]
+    public string[] buttonDescription;
+
+    private void Awake()
+    {
+        ChoosingAction.AddListener(OnChoosingAction);
+        ChoseAction.AddListener(OnChoseAction);
+    }
+
+    //Testing
+    private void Start()
+    {
+        ChoosingAction.Invoke();
+    }
+
+    private void OnChoosingAction()
+    {
+        buttonDescriptionTMProTarget.text = "";
+
+        ButtonSelected.AddListener(OnButtonSelected);
+        ButtonDeselected.AddListener(OnButtonDeselected);
+    }
+
+    private void OnChoseAction()
+    {
+        ButtonSelected.RemoveListener(OnButtonSelected);
+        ButtonDeselected.RemoveListener(OnButtonDeselected);
+
+        StartCoroutine(MoveDecisionsAway(true));
+    }
+
+    private void OnButtonDeselected()
+    {
+        StopAllCoroutines();
+        _typingRoutine = null;
+
+        buttonDescriptionTMProTarget.text = "";
+    } 
+
+    private void OnButtonSelected(int buttonID)
+    {
+        if(_typingRoutine == null)
+        {
+            _typingRoutine = StartCoroutine(TypeText(textSpeed, buttonDescription[buttonID]));
+        }
+    }
+
+    private IEnumerator TypeText(float speed, string text)
+    {
+        buttonDescriptionTMProTarget.text = "";
+
+        foreach(var c in text)
+        {
+            buttonDescriptionTMProTarget.text += c;
+
+            yield return new WaitForSeconds(speed / text.Length);
+        }
+
+        _typingRoutine = null;
+    }
+
+    private IEnumerator MoveDecisionsAway(bool isAway)
+    {
+        Vector3 dir = Vector3.down * 100;
+
+        if(!isAway)
+        {
+            dir = -dir;
+        }
+
+        for(float i = 0; i < 1.1; i += Time.deltaTime)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + dir, i);
+
+            yield return null;
+        }
+    }
+}
